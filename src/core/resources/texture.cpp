b2db4823e0f67ec26731a2fc646e47807b47ca16
Stablest <marlonlocci@hotmail.com>
2025-09-29 23:53:50 -0300

    feat: add texture resource

--- src/core/resources/texture.cpp
+++ src/core/resources/texture.cpp
@@ -1,13 +1,8 @@
 #include "texture.hpp"
-
-#include <iostream>
-
 #include "glad/glad.h"
 
-Texture2D::Texture2D(const unsigned char *data) {
+Texture2D::Texture2D(const unsigned char *data, const GLsizei width, const GLsizei height, const int channels) {
     TextureID id;
-    constexpr int width = 0;
-    constexpr int height = 0;
     glGenTextures(1, &id);
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, id);
@@ -17,12 +12,14 @@ Texture2D::Texture2D(const unsigned char *data) {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     //
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 32, 32, 0, GL_RGBA,
+    const GLenum format = (channels == 4) ? GL_RGBA : GL_RGB;
+    const GLenum internalFormat = (channels == 4) ? GL_RGBA8 : GL_RGB8;
+    glTexImage2D(GL_TEXTURE_2D, 0, static_cast<GLint>(internalFormat), width, height, 0, format,
                  GL_UNSIGNED_BYTE, data);
     glGenerateMipmap(GL_TEXTURE_2D);
     this->id_ = id;
-    this->width_ = 32;
-    this->height_ = 32;
+    this->width_ = width;
+    this->height_ = height;
 }
 
 void Texture2D::bind() const {
--- src/core/resources/texture.hpp
+++ src/core/resources/texture.hpp
@@ -1,15 +1,15 @@
 #pragma once
+#include "glad/glad.h"
 
 using TextureID = unsigned int;
 
 class Texture2D {
     TextureID id_ = 0;
     unsigned int width_ = 0, height_ = 0;
-    unsigned int imageFormat_ = 0;
 public:
     unsigned int wrapS_ = 0, wrapT_ = 0;
     unsigned int filterMin_ = 0, filterMax_ = 0;
-    explicit Texture2D(const unsigned char* data);
+    explicit Texture2D(const unsigned char *data, GLsizei width, GLsizei height, int channels);
 
     inline void bind() const;
 
